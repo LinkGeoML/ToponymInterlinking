82e1a5a57cb3ced515b3a8b3577910a5a229961e
vkaff <vkaffes@imis.athena-innovation.gr>
2019-04-16 00:57:43 +0300

    set the correct global dataset name

--- scripts/basic_train_global.sh
+++ scripts/basic_train_global.sh
@@ -3,8 +3,8 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 echo "Comparison-based interlinking..."
-python ../feml.py -d datasets/dataset-string-similarity_latin_EU_NA_100k.txt -e all
+python ../feml.py -d datasets/dataset-string-similarity_original_100k.csv -e all
 
 echo ""
 echo "Classification-based interlinking..."
-python ../feml.py -d datasets/dataset-string-similarity_latin_EU_NA_100k.txt --ev customFEML -e all
+python ../feml.py -d datasets/dataset-string-similarity_original_100k.csv --ev customFEML -e all
--- scripts/lgm_train_global.sh
+++ scripts/lgm_train_global.sh
@@ -3,8 +3,8 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 echo "Comparison-based interlinking..."
-python ../feml.py -d datasets/dataset-string-similarity_latin_EU_NA_100k.txt -e all --sort --canonical
+python ../feml.py -d datasets/dataset-string-similarity_original_100k.csv -e all --sort --canonical
 
 echo ""
 echo "Classification-based interlinking..."
-python ../feml.py -d datasets/dataset-string-similarity_latin_EU_NA_100k.txt --ev customFEMLExtended -e all --sort --canonical
+python ../feml.py -d datasets/dataset-string-similarity_original_100k.csv --ev customFEMLExtended -e all --sort --canonical
